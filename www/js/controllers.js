288ad16b58573b4843a9cb7d05935b592094035d
Mindtree PSG LMS <mindtree.psg.lms@gmail.com>
2015-09-23 06:54:19 +0000

    changed controller js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -48,7 +48,9 @@ angular.module('starter.controllers', [])
     { title: 'Dubstep', id: 3 },
     { title: 'Indie', id: 4 },
     { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Cowbell', id: 6 },
+     { title: 'Sanjeev', id: 7 },
+      { title: 'Asfiya', id: 8 }
   ];
 })
 
